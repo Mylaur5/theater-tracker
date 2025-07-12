5bc934b7a5711d7b9fb028eb36947b6830a4ff3f
Minh-Hoang HUYNH <minh-hoang.huynh@outlook.fr>
2025-07-12 11:51:56 +0200

    Add a Refresh Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,15 +82,17 @@
 					<path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
 				</svg>`
 		},
-		// {
-		// 	onclick: refreshData,
-		// 	label: 'Refresh',
-		// 	iconSVG: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
-		// 		<path
-		// 			d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"
-		// 		/>
-		// 	</svg>`
-		// },
+		{
+			onclick: () => {
+				window.open('https://github.com/Mylaur5/theater-tracker/actions/workflows/deploy-gh-pages.yml', '_blank');
+			},
+			label: 'Refresh',
+			iconSVG: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+				<path
+					d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"
+				/>
+			</svg>`
+		},
 		{
 			onclick: nextSeason,
 			label: 'Next',
