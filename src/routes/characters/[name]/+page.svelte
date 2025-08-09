078f0b1bc8592d8b935db51ec0130f746803b090
Minh-Hoang HUYNH <minh-hoang.huynh@outlook.fr>
2025-08-09 17:24:14 +0200

    Fix 2

--- src/routes/characters/[name]/+page.svelte
+++ src/routes/characters/[name]/+page.svelte
@@ -134,7 +134,7 @@
 		{/if}
 		<button
 			type="button"
-			class="m-6 flex h-8 items-center self-end rounded-md border p-3 px-4 text-white hover:bg-gray-200 focus:outline-none focus:ring-2 border-green-700 bg-green-600 dark:hover:bg-green-800"
+			class="m-6 flex h-8 items-center self-end rounded-md border p-3 px-4 text-white focus:outline-none focus:ring-2 border-green-700 bg-green-600 hover:bg-green-800"
 			onclick={saveCharToGoodFile}
 		>
 			Save
